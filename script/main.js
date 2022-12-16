d0e3e1a26e2556237fc410cc18f179010c9ae71f
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2022-12-16 15:08:34 +0100

    floor done

--- script/main.js
+++ script/main.js
@@ -5,10 +5,27 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 canvas.height = 1000
-canvas.width = canvas.height * 10 / 16
+canvas.width = canvas.height * 16 / 10
+
+class ground {
+    constructor() {
+        this.yPosition = canvas.height * 8 / 10
+        this.height = canvas.height * 2 / 10
+    }
+
+    draw = () => {
+        ctx.fillStyle = "#252525"
+        ctx.fillRect(0, this.yPosition, canvas.width, this.height)
+    }
+}
+
+const lowerBound = new ground()
 
 const frame = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+    lowerBound.draw()
+    
     window.requestAnimationFrame(frame)
 }
 
